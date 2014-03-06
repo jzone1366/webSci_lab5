0a06aa8bcd5091333b680df800344e21152061bc
zonej <jzone1229@gmail.com>
2014-03-06 14:53:55 -0500

    Fix write to add commas between data segments. Also need to remove the last comma and add square brackets at beginning and end. Writes 1000 tweets to the json file to be used.

--- tweets.js
+++ tweets.js
@@ -19,21 +19,39 @@ console.log('Listening on http://127.0.0.1:8000');
 
 var sw = '-73.68,42.72', ne = '-73.67,42.73';
 var options = {encoding: 'utf8'};
-var writeStream = fs.createWriteStream('out.json');
+var writeStream = fs.createWriteStream('ITWS4200-lab5-zonej.json');
 var count = 1;
+
+writeStream.write('[');
 
 twit.stream('statuses/filter', {'locations':sw + ',' + ne},
 	function(stream) {
+
 		stream.on('error', function(error, code) {
   				 console.log("My error: " + error + ": " + code);
 		});
+		
 		stream.on('data', function (data) {
 			jdata = JSON.stringify(data, null, 4);
+			
+			writeStream.setMaxListeners(0);
+			
 			writeStream.on('error', function(err) {
 				console.log(err);
 			});
-			writeStream.write(jdata);
-			console.log(count);
+
+			writeStream.write(jdata + ",");
+			
+			if(count % 10 == 0) {
+				console.log(count);
+			}
+
+			if(count == 1000) {
+				writeStream.write(']');
+
+				writeStream.end();
+			}
+
 			count++;
 		});
 });
